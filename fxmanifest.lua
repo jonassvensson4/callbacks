651362e97debabe0a10410ecba33378d5324f501
Jonas Svensson <jonassvensson4@hotmail.com>
2020-02-14 16:17:05 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+server_script 'server.js'
+client_script 'client.js'
+
+export 'serverCallback'
+server_export 'clientCallback'
---
+++ server.js
@@ -0,0 +1,23 @@
+let callbacks = {};
+
+RegisterNetEvent('callbacks:server')
+onNet('callbacks:server', ( result, id ) => {
+    if ( id >= 0 ) {
+        callbacks[id](result);
+        delete callbacks[id];
+    } else {
+        console.error('No callback ID set');
+    }
+});
+
+/**
+ * @param {string} name - The name of the server event
+ * @param {object} data - The object that contains data that you send to the server event
+ * @param {function} cb - The callback that handles the response
+ */
+function clientCallback( name, player, data, cb ) {
+    let id = Object.keys( callbacks ).length++;
+    callbacks[id] = cb;
+    data['CallbackID'] = id;
+    emitNet(name, player, data);
+}
